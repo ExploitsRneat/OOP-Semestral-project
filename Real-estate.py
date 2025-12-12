f2a461c8001dea6a5e0c37b557439f0dcfef2b6b
zackkkk <emmanuelbriansandoval225@gmail.com>
2025-12-12 23:31:48 +0800

    Update Real-estate.py
    This file is now invalid as the real Real-estate.py is divided into three separate files. It retains project history for reference.

--- Real-estate.py
+++ Real-estate.py
@@ -1,2 +1,3 @@
 #This file is now invalid as the real real-estate.py is divided into three. 
 #Can't delete this as it holds 3-weeks of project history
+#Open Real Estate Project folder to see the project
